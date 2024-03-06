2f2fdcd377a34b71425edcfb63188db1a76525e2
Sarah Nadeem <1100063811@edu.cbe.ab.ca>
2024-03-06 14:41:34 -0700

    buuble sort

--- bubble-sort.js
+++ bubble-sort.js
@@ -8,6 +8,15 @@ function bubbleSort(array) {
     ** Repeat the above process until you get through an entire
     ** cycle without needing any swaps
     */
+   for (let i = 0; i < array.length; i++){
+    for (let j = 0; j< (arr.length - i - 1); j++){
+        if (arr[j] > arr[j+1]){
+            let temp = arr[j]
+            arr[j+1]
+            arr[j+1] = temp
+        }
+    }
+   }
 
 	return array;
 }
